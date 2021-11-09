4b0db477f9af11750500ac4da1706fa40942dbe1
stanlesuorec <93797774+stanlesuorec@users.noreply.github.com>
2021-11-09 03:30:41 +0300

    Add files via upload

--- stanle.lua
+++ stanle.lua
@@ -4454,7 +4454,7 @@ Text = [[
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '✫  𝘀𝘁𝗮𝗻𝗹𝗲 ᴄʜᴀɴɴᴇʟ',url="https://t.me/TRR8T"}},
-{{text = '✫  𝘀𝘁𝗮𝗻𝗹𝗲 ᴅᴇᴠᴇʟᴏᴘᴇʀ',url="t.me/NNLNNN"},{text = '✫  ʙᴏᴛ 𝘀𝘁𝗮𝗻𝗹𝗲',url="https://t.me/SiTmelanobot"}},{{text= '✫  ɪɴғᴏ 𝘀ᴏᴜʀᴄᴇ .',url="https://t.me/MiLaNoTam"}}
+{{text = '✫  𝘀𝘁𝗮𝗻𝗹𝗲 ᴅᴇᴠᴇʟᴏᴘᴇʀ',url="t.me/NNLNNN"},{text = '✫  ʙᴏᴛ 𝘀𝘁𝗮𝗻𝗹𝗲',url="https://t.me/SiTmelanobot"}},{{text= '✫  ɪɴғᴏ 𝘀ᴏᴜʀᴄᴇ .',url="https://t.me/stanletame"}}
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/TRR8T&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
